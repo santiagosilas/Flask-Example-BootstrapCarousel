296ae1a93f65d268c635cef6d7a9883eb24448f6
Silas (Notebook) <santiagosilas@outlook.com>
2021-09-30 09:29:21 -0300

    Example Carousel Bootstrap

---
+++ example-carousel-bs/app/views.py
@@ -0,0 +1,8 @@
+﻿# exemplo
+from flask import Flask, request, render_template,redirect, url_for
+from app import app 
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
